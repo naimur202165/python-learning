96d5a9d0fcebd9fc8ac0c8d6dae6a42b480ac83b
naimur <naimur202165@gmail.com>
2025-02-14 14:46:23 +0600

    learning python

---
+++ basic/index.py
@@ -0,0 +1,13 @@
+print("hello")
+
+print("Hello world");
+
+
+if 5>4 :
+    print("5 is  biger than 4")
+
+
+
+
+# this is how to commnent on the python code base 
+#we all are alble to do that thing
